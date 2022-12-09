ebf7accd70a7c251ec4e0cb5f4066a388699ba24
Mathias Korte <mathias.korte@tum.de>
2022-12-09 03:57:29 +0100

    (try to) add geometry oblivious clustering

--- src/structurednets/approximators/hodlr_approximator.py
+++ src/structurednets/approximators/hodlr_approximator.py
@@ -1,10 +1,51 @@
 import numpy as np
-
+from sklearn.cluster import AgglomerativeClustering
 from structurednets.approximators.approximator import Approximator
 from structurednets.hmatrix.block_cluster_tree import BlockClusterTree
 from structurednets.hmatrix.tree_element import TreeElement
 from structurednets.hmatrix.hmatrix import HMatrix
 from structurednets.models.googlenet import GoogleNet
+
+def permuate_clusters(M, depth=5):
+    def get_even_clusters(X):     
+        # distance_matrix = np.array([[(X[i,i]+X[j,j]-2*X[i,j]) for i in range(X.shape[0])] for j in range(X.shape[0])])
+        distance_matrix = np.square([[np.linalg.norm(X[i,:]-X[j,:]) for i in range(X.shape[0])] for j in range(X.shape[0])])
+        clusters = AgglomerativeClustering(n_clusters=2, metric='precomputed',linkage='complete').fit_predict(distance_matrix)
+        for idx in np.argsort(distance_matrix[np.where(clusters == 1)[0][0],:]):
+            if(np.sum(clusters==0)<np.sum(clusters==1)+1):
+                break
+            clusters[idx]=1
+        return clusters
+
+    for d in range(depth):
+            block_num = int(2**d)
+            xd = (M.shape[0]/block_num)
+            yd = (M.shape[1]/block_num)
+
+            x_perm_list=[]
+            y_perm_list=[]
+            for b in range(block_num):
+
+                if (b+1<block_num):
+                    B = M[int(b*xd):int((b+1)*xd), int(b*yd):int((b+1)*yd)]
+                else:
+                    B = M[int(b*xd):, int(b*yd):]
+
+                x_part_perm = np.argsort(get_even_clusters(B))
+                y_part_perm = np.argsort(get_even_clusters(B.T))             
+                
+                x_part_perm += int(b*xd)
+                y_part_perm += int(b*yd)
+
+                x_perm_list.append(x_part_perm)
+                y_perm_list.append(y_part_perm)
+
+            x_perm = np.concatenate(x_perm_list)
+            y_perm = np.concatenate(y_perm_list) 
+
+            M = M[x_perm,:]
+            M = M[:,y_perm]
+    return M
 
 def build_hodlr_block_cluster_tree(depth: int, matrix_shape: tuple, min_block_size=2) -> BlockClusterTree:
     root = TreeElement(children=None, row_range=range(matrix_shape[0]), col_range=range(matrix_shape[1]))
@@ -42,16 +83,22 @@ class HODLRApproximator(Approximator):
         return "HODLRApproximator"
 
 if __name__ == "__main__":
-    tree = build_hodlr_block_cluster_tree(8, (100, 100))
-    tree.plot()
+    # tree = build_hodlr_block_cluster_tree(8, (100, 100))
+    # tree.plot()
 
-    model = GoogleNet(output_indices=np.arange(1000), use_gpu=False)
-    optim_mat = model.get_optimization_matrix().detach().numpy()
+    # model = GoogleNet(output_indices=np.arange(1000), use_gpu=False)
+    # optim_mat = model.get_optimization_matrix().detach().numpy()
+
+    optim_mat = np.random.rand(100,100)    
 
     approximator = HODLRApproximator()
     res = approximator.approximate(optim_mat=optim_mat, nb_params_share=0.4)
     print("Approximation Error: " + str(np.linalg.norm(res["approx_mat_dense"] - optim_mat, ord="fro")))
 
-    hmatrix = res["h_matrix"]
-    nb_hmatrix_components = len(hmatrix.block_cluster_tree.get_all_hmatrix_components())
-    print("Nb Components: " + str(nb_hmatrix_components))
+    print("permuting based on clustering...")
+    optim_mat = permuate_clusters(optim_mat)
+
+    approximator = HODLRApproximator()
+    res = approximator.approximate(optim_mat=optim_mat, nb_params_share=0.4)
+    print("Approximation Error: " + str(np.linalg.norm(res["approx_mat_dense"] - optim_mat, ord="fro")))
+
